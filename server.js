d8e64790c5b5f4dc446f6181a79aa67df4941f92
kj0628 <kevinjin06284@gmail.com>
2025-06-17 16:08:36 +0900

    feat: Delete journal API

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
 
+
+import journalsRouter from './src/domains/journals/routes/journalRoutes.js'; // 저널 관련 라우터 임포트
+
+
 // 환경 변수 사용준비
 dotenv.config();
 
@@ -12,6 +16,9 @@ const app = express();
 app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
+
+
+app.use('/journals', journalsRouter); // 저널 관련 라우터 등록
 
 // 테스트용 라우터
 app.get('/', (req, res) => {
