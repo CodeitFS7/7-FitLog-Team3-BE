a6d79d13f3ae6378c8f0f152555c4e92c9166920
zorbakim <hunahpu.vuch@gmail.com>
2025-06-18 09:37:51 +0900

    feat:exercise post api

--- src/domains/exercise-logs/routes/index.js
+++ src/domains/exercise-logs/routes/index.js
@@ -1,3 +1,18 @@
-import { Router } from "express";
-import exerciselogController from "../controller/index";
+import { Router } from 'express';
+import ExerciseLogController from '../controller/index.js';
+import ExerciseLogService from '../service/index.js';
+import ExerciseLogRepository from '../repository/index.js';
+import prisma from '../prismaClient.js';
+
+export const exerciseLogRouter = Router();
+
+// repository → service → controller
+const repository = new ExerciseLogRepository(prisma);
+const service = new ExerciseLogService(repository);
+const controller = new ExerciseLogController(service);
+
+// POST /exercise-log
+router.post('/exercise-log', (req, res, next) => {
+  controller.create(req, res, next);
+});
 
