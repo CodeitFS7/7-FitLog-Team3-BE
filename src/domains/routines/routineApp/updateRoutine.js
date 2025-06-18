ce3478124571e4531f0852212b5f1f5b11823841
ehj <ehj9118@gmail.com>
2025-06-18 15:37:36 +0900

    feat: Use Cases(appRoutine) 로직 작성

--- src/domains/routines/routineApp/updateRoutine.js
+++ src/domains/routines/routineApp/updateRoutine.js
@@ -1,7 +1,47 @@
+import { routinesRepository } from '../repository/routinesRepository.js';
+
 class UpdateRoutine {
     constructor(updateRoutine) {
       this.updateRoutine = updateRoutine;
     }
+  /* 루틴 존재 여부 확인*/ 
+      async execute(routineId, updateData) {
+        const existingRoutine = await this.routinesRepository.findById(routineId);
+
+        if (!existingRoutine) {
+            throw new Error('업데이트할 루틴을 찾을 수 없습니다.');
+        }
+    /* 변경사항 여부 확인*/ 
+        let hasChanges = false;
+        const dataToUpdate = {};
+
+        if (updateData.title !== undefined) { 
+            if (updateData.title !== existingRoutine.title) {
+                const existingRoutineWithSameTitle 
+                = await this.routinesRepository.findByJournalIdAndTitle(
+                    existingRoutine.journalId, 
+                    updateData.title
+                );
+
+                if (existingRoutineWithSameTitle) {
+                    throw new Error('일지에 이미 존재하는 루틴입니다.');
+                }
+
+                 hasChanges = true;
+                dataToUpdate.title = updateData.title;
+            }
+        }
+        
+         if (!hasChanges) {
+            return { message: "변경 사항이 존재하지 않습니다", routine: existingRoutine };
+        }
+
+     try {
+            const updatedRoutine = await this.routinesRepository.findByIdAndUpdate(routineId, dataToUpdate);
+            return updatedRoutine;
+        } catch (error) 
+        throw error;
+        }
 }
 
 export default UpdateRoutine;
