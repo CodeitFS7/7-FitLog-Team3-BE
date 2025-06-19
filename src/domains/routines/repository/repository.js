4324a1829cdd9a7d6952e96e7a59802cf7f4f82c
Lee Yujin <yuwls1102@gmail.com>
2025-06-19 17:22:05 +0900

    fix: rebase 이후 에러 수정 및 중복 파일 정리

--- src/domains/routines/repository/repository.js
+++ src/domains/routines/repository/repository.js
@@ -1,7 +1,29 @@
-import { PrismaClient } from '@prisma/client';
-const prisma = new PrismaClient();
+import { prisma } from '../../utils/prisma.js';
 
-export class RoutineRepository {
+export class RoutinesRepository {
+  /* 루틴 생성 */
+  async create(data) {
+    return await prisma.routine.create({ data });
+  }
+
+  /* 루틴 업데이트 */
+  async findByIdAndUpdate(id, updateData) {
+    return await this.prisma.routine.update({
+      where: { id },
+      data: updateData,
+    });
+  }
+
+  /* 중복 생성 방지 */
+  async findByJournalIdAndTitle(journalId, title) {
+    return await this.prisma.routine.findFirst({
+      where: {
+        journalId: journalId,
+        title: title,
+      },
+    });
+  }
+
   async findByJournalId(journalId) {
     return prisma.routine.findMany({
       where: { journalId },
--- src/domains/routines/repository/routinesRepository.js
+++ src/domains/routines/repository/routinesRepository.js
@@ -1,32 +1,32 @@
-import { PrismaClient } from '@prisma/client';
-const prisma = new PrismaClient();
+// import { PrismaClient } from '@prisma/client';
+// const prisma = new PrismaClient();
 
-class RoutinesRepository { 
-    constructor() {
-        this.prisma = prisma; 
-    }
-/* 루틴 생성 */
-  async create(data) {
-    return await prisma.routines.create({ data });
-  }
+// class RoutinesRepository {
+//     constructor() {
+//         this.prisma = prisma;
+//     }
+// /* 루틴 생성 */
+//   async create(data) {
+//     return await prisma.routines.create({ data });
+//   }
 
-  /* 루틴 업데이트 */ 
-  async findByIdAndUpdate(id, updateData) {
-        return await this.prisma.routine.update({
-            where: { id },
-            data: updateData,
-        });
-    }
+//   /* 루틴 업데이트 */
+//   async findByIdAndUpdate(id, updateData) {
+//         return await this.prisma.routine.update({
+//             where: { id },
+//             data: updateData,
+//         });
+//     }
 
-/* 중복 생성 방지 */
-     async findByJournalIdAndTitle(journalId, title) {
-        return await this.prisma.routine.findFirst({ 
-            where: {
-                journalId: journalId,
-                title: title,
-            },
-        });
-    }
-}
+// /* 중복 생성 방지 */
+//      async findByJournalIdAndTitle(journalId, title) {
+//         return await this.prisma.routine.findFirst({
+//             where: {
+//                 journalId: journalId,
+//                 title: title,
+//             },
+//         });
+//     }
+// }
 
-export { RoutinesRepository }
+// export { RoutinesRepository }
