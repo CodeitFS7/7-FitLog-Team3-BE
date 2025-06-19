4a4e40fe84ec1904d15603bee0f4db2af6a9b67a
Lee Yujin <yuwls1102@gmail.com>
2025-06-20 03:38:28 +0900

    fix: 루틴 get,delete API 수정

--- src/domains/routines/service/service.js
+++ src/domains/routines/service/service.js
@@ -74,17 +74,18 @@ export class RoutinesService {
   };
 
   getAllRoutines = async (journalId) => {
-    try {
-      return await this.routinesRepository.findJournalById(journalId);
-    } catch (err) {
-      const error = new Error('루틴 조회 실패');
-      error.status = 500;
+    // 일지에 해당하는 루틴을 가져왔을때, 루틴이 없을 경우 에러
+    const routines = await this.routinesRepository.findRoutineByJournalId(journalId);
+    if (!routines) {
+      const error = new Error('루틴이 존재하지 않습니다.');
+      error.status = 404;
       throw error;
     }
+    return routines;
   };
 
-  deleteRoutine = async (id, userId) => {
-    const routine = await this.routinesRepository.findById(id);
+  deleteRoutineById = async (journalId, routineId) => {
+    const routine = await this.routinesRepository.findRoutineById(routineId);
 
     if (!routine) {
       const error = new Error('루틴이 존재하지 않습니다');
@@ -92,18 +93,12 @@ export class RoutinesService {
       throw error;
     }
 
-    if (routine.journal.userId !== userId) {
-      const error = new Error('해당 루틴에 대한 권한이 없습니다');
-      error.status = 403;
+    if (routine.journalId !== journalId) {
+      const error = new Error('이 루틴을 삭제할 권한이 없습니다.');
+      error.statusCode = 403; // Forbidden (금지됨)
       throw error;
     }
 
-    try {
-      return await this.routinesRepository.deleteById(id);
-    } catch (err) {
-      const error = new Error('루틴 삭제 실패');
-      error.status = 500;
-      throw error;
-    }
+    return await this.routinesRepository.deleteRoutineById(routineId);
   };
 }
