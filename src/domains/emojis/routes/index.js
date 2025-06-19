73dd3600b8abb38ae75491ab3a1a777722c46168
Lee Yujin <yuwls1102@gmail.com>
2025-06-19 09:13:45 +0900

    feat: 이모지 Get API 구현

--- src/domains/emojis/routes/index.js
+++ src/domains/emojis/routes/index.js
@@ -4,6 +4,7 @@ import { EmojisService } from '../service/index.js';
 import { EmojisRepository } from '../repository/index.js';
 import { JournalsRepository } from '../../journals/repository/index.js';
 import { validatePostEmoji } from '../../middlewares/validatePostEmoji.js';
+import { validateGetEmojisByJournalId } from '../../middlewares/validateGetEmojisByJournalId.js';
 
 export const emojisRouter = Router({ mergeParams: true });
 
@@ -13,3 +14,4 @@ const emojisService = new EmojisService(emojisRepository, journalsRepository);
 const emojisController = new EmojisController(emojisService);
 
 emojisRouter.post('/', validatePostEmoji, emojisController.addOrUpdateEmoji);
+emojisRouter.get('/', validateGetEmojisByJournalId, emojisController.getEmojisByJournalId);
