6a7d9dbc08566565fe250129d0aa3ebc4903dcc8
OhHaesang <nan1004388@gmail.com>
2025-06-19 13:50:20 +0900

    feat: 의존성 주입 및 class형 변경

--- src/domains/routines/service/service.js
+++ src/domains/routines/service/service.js
@@ -1,41 +1,39 @@
-import routineRepository from '../repository/repository.js';
-
-const getAllRoutines = async () => {
-  try {
-    return await routineRepository.findAll();
-  } catch (err) {
-    const error = new Error('루틴 조회 실패');
-    error.status = 500;
-    throw error;
-  }
-};
-
-const deleteRoutine = async (id, userId) => {
-  const routine = await routineRepository.findById(id);
-
-  if (!routine) {
-    const error = new Error('루틴이 존재하지 않습니다');
-    error.status = 404;
-    throw error;
+export class RoutineService {
+  constructor(routineRepository) {
+    this.routineRepository = routineRepository;
   }
 
-  // 권한 검사 예시 (루틴 생성자만 삭제 가능)
-  if (routine.userId !== userId) {
-    const error = new Error('해당 루틴에 대한 권한이 없습니다');
-    error.status = 403;
-    throw error;
+  async getAllRoutines(journalId) {
+    try {
+      return await this.routineRepository.findByJournalId(journalId);
+    } catch (err) {
+      const error = new Error('루틴 조회 실패');
+      error.status = 500;
+      throw error;
+    }
   }
 
-  try {
-    return await routineRepository.deleteById(id);
-  } catch (err) {
-    const error = new Error('루틴 삭제 실패');
-    error.status = 500;
-    throw error;
-  }
-};
+  async deleteRoutine(id, userId) {
+    const routine = await this.routineRepository.findById(id);
 
-export default {
-  getAllRoutines,
-  deleteRoutine,
-};
+    if (!routine) {
+      const error = new Error('루틴이 존재하지 않습니다');
+      error.status = 404;
+      throw error;
+    }
+
+    if (routine.journal.userId !== userId) {
+      const error = new Error('해당 루틴에 대한 권한이 없습니다');
+      error.status = 403;
+      throw error;
+    }
+
+    try {
+      return await this.routineRepository.deleteById(id);
+    } catch (err) {
+      const error = new Error('루틴 삭제 실패');
+      error.status = 500;
+      throw error;
+    }
+  }
+}
