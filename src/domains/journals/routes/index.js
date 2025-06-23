a035d7a92d52bf2a348c7288bbf44610d38d742a
Lee Yujin <yuwls1102@gmail.com>
2025-06-23 17:14:51 +0900

    feature: 저널 비밀번호 검사 api 구현

--- src/domains/journals/routes/index.js
+++ src/domains/journals/routes/index.js
@@ -19,5 +19,6 @@ journalsRouter.get('/', validateGetJournalsQuery, controller.getJournals);
 journalsRouter.get('/:journalId', validateJournalIdParam, controller.getJournalById);
 journalsRouter.patch('/:journalId', validatePatchJournals, controller.updateJournalById);
 journalsRouter.delete('/:journalId', validateJournalIdParam, controller.deleteJournalById);
+journalsRouter.post('/:journalId/verifyJournalPassword', controller.verifyJournalPassword);
 
 journalsRouter.use('/:journalId/emojis', emojisRouter);
