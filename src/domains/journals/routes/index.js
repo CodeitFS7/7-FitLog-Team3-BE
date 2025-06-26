38cc2f8f593d8f12940edd4649b4879cf7f07c72
Lee Yujin <yuwls1102@gmail.com>
2025-06-26 13:04:35 +0900

    fix: 저널 조회 및 업데이트 api 수정

--- src/domains/journals/routes/index.js
+++ src/domains/journals/routes/index.js
@@ -2,6 +2,8 @@ import { Router } from 'express';
 import { JournalsController } from '../controller/index.js';
 import { JournalsService } from '../service/index.js';
 import { JournalsRepository } from '../repository/index.js';
+import ExerciseLogsService from '../../exercise-logs/service/index.js';
+import ExerciseLogsRepository from '../../exercise-logs/repository/index.js';
 import { validateCreateJournal } from '../../middlewares/validateJournalCreation.js';
 import { validateGetJournalsQuery } from '../../middlewares/validateGetJournalsQuery.js';
 import { validateJournalIdParam } from '../../middlewares/validateJournalIdParam.js';
@@ -12,7 +14,9 @@ import { validatePostVerifyJournalPassword } from '../../middlewares/validatePos
 export const journalsRouter = Router();
 
 const repository = new JournalsRepository();
-const service = new JournalsService(repository);
+const exerciseLogsRepository = new ExerciseLogsRepository();
+const exerciseLogsService = new ExerciseLogsService(exerciseLogsRepository, repository);
+const service = new JournalsService(repository, exerciseLogsService);
 const controller = new JournalsController(service);
 
 journalsRouter.post('/', validateCreateJournal, controller.createJournal);
